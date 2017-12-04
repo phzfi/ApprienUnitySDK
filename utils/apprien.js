d1abd978c8a3bcf07ca06608ba63d17d49608686
Antti Hätinen <antti.hatinen@phz.fi>
2017-12-04 16:06:54 +0200

    Fixed parsing offset. some initial test case samples

--- utils/apprien.js
+++ utils/apprien.js
@@ -3,7 +3,7 @@
 	 * Parses the base SKU name (used by the game to display the graphic assets and 
 	 * to deliver the goods to the player) based on the Apprien response (variant SKU)
 	 *
-	 * Variant SKU is e.g. z_base_sku_name.apprien_500_dfa3, where 
+	 * Variant SKU is e.g. z_base_sku_name.apprien_500_dfa3, where
 	 * - the prefix is z_ (2 characters) to sort the skus on store listing to then end
 	 * - followed by the base sku name that can be parsed by splitting the string by separator ".apprien_"
 	 * - followed by the price in cents
@@ -17,7 +17,7 @@
 		//first check if this is a variant sku or base sku
 		var apprienSeparatorPosition = storeSku.indexOf(".apprien_");
 		if (apprienSeparatorPosition > 0) {
-			result = storeSku.substring(2, storeSku.length -2);
+			result = storeSku.substring(2, storeSku.length -1);
 			result = result.substring(0, result.length - apprienSeparatorPosition);
 		}
 		return result;
---
+++ utils/test.js
@@ -0,0 +1,2 @@
+getBaseSku("base_sku")
+getBaseSku("z_base_sku.apprien_1180_zydc")
