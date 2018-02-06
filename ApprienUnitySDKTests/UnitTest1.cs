e2dc97a924716dda88cdffa1ab27b18ad4d6273a
Antti Hätinen <pharazon@phz.fi>
2018-02-06 04:37:22 +0200

    Created a test project, moved test dependencies to test project

---
+++ ApprienUnitySDKTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ApprienUnitySDKTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(null, null);
+        }
+    }
+}
