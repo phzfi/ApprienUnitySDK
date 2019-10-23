285bdee49a4cfd57a4df65372266cc0a6e4f2f25
Henri Niva <henri.niva@phz.fi>
2019-10-23 16:06:51 +0300

    WIP on CI exporting unity packages automatically

---
+++ unity5-export-package.sh
@@ -0,0 +1,2 @@
+set -x
+/opt/Unity/Editor/Unity -batchmode -quit -projectPath /root/project/ApprienUnitySDK -nographics  -exportPackage Assets/Apprien /root/project/UnityPackages/apprien-unity-sdk-5-6-3.unityPackage
