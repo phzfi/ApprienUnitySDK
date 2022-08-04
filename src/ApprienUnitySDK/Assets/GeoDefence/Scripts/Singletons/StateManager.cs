66b4efb9820ccacd791ebf04b549dbed34686003
Lari Miettinen <lari.miettinen@phz.fi>
2022-08-04 16:03:12 +0300

    Added Statemanager and transition manager singletons to singleton loader

--- src/ApprienUnitySDK/Assets/GeoDefence/Scripts/Singletons/StateManager.cs
+++ src/ApprienUnitySDK/Assets/GeoDefence/Scripts/Singletons/StateManager.cs
@@ -2,17 +2,249 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateManager : MonoBehaviour
+namespace GeoDefence
 {
-    // Start is called before the first frame update
-    void Start()
+    public class StateManager : MonoBehaviour
     {
-        
-    }
+        public static StateManager Instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public GameState CurrentGameState { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
+
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace gbpickup
+{
+    public class StateManager : MonoBehaviour
+    {
+        public static StateManager Instance;
+
+        [SerializeField] private GameState _currentGameState;
+        [SerializeField] private GameState _nextGameState;
+        [SerializeField] private SubState _subState;
+        [SerializeField] private SceneName _nextSceneName;
+        [SerializeField] private bool _inTransition;
+
+        public bool DifficultySelected;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            switch (_currentGameState)
+            {
+                case GameState.Loading:
+                    LoadingState();
+                    break;
+                case GameState.DifficultySelection:
+                    DifficultySelectionState();
+                    break;
+                case GameState.ModelShowDown:
+                    ModelShowDownState();
+                    break;
+                case GameState.PickUp:
+                    PickUpState();
+                    break;
+                case GameState.RoundScore:
+                    RoundScoreState();
+                    break;
+            }
+
+            if (_currentGameState != _nextGameState)
+            {
+                if (TransitionManager.Instance.GetInTransition() && TransitionManager.Instance.GetInMiddlePoint())
+                {
+                    ExecuteStateTransition();
+                }
+                else if (!_inTransition)
+                {
+                    ExecuteStateTransition();
+                }
+            }
+        }
+
+        public void ChangeGameState(GameState nextGameState, float transitionTimeInSeconds, SceneName nextScene = SceneName.None, bool instant = true,
+            TransitionMessage message = TransitionMessage.None, bool particles = false)
+        {
+            TransitionManager.Instance.OrderTransition(transitionTimeInSeconds, message, particles);
+            _nextGameState = nextGameState;
+            _nextSceneName = nextScene;
+            _inTransition = !instant;
+        }
+
+        private void ExecuteStateTransition()
+        {
+            _currentGameState = _nextGameState;
+            _subState = SubState.BeginState;
+            if (_nextSceneName != SceneName.None)
+            {
+                SceneManager.LoadScene(_nextSceneName.ToString());
+                _nextSceneName = SceneName.None;
+                _inTransition = false;
+            }
+        }
+
+        private void LoadingState()
+        {
+            ChangeGameState(GameState.DifficultySelection, 0f, SceneName.DifficultySelection);
+        }
+
+        private void DifficultySelectionState()
+        {
+            switch (_subState)
+            {
+                case SubState.BeginState:
+                    GlobalUIManager.Instance.ShowTimeBar = false;
+                    CoreLoopManager.Instance.DisableScoreView();
+                    _subState = SubState.RunState;
+                    break;
+                case SubState.RunState:
+                    if (DifficultySelected)
+                    {
+                        _subState = SubState.ExitState;
+                    }
+                    break;
+                case SubState.ExitState:
+                    if (DifficultySelected)
+                    {
+                        CoreLoopManager.Instance.CreateGame(true);
+                        ChangeGameState(GameState.ModelShowDown, 5f, SceneName.Coreloop, false, TransitionMessage.GetReady, true);
+                        DifficultySelected = false;
+                    }
+                    break;
+
+            }
+        }
+
+        private void ModelShowDownState()
+        {
+            switch (_subState)
+            {
+                case SubState.BeginState:
+                    CoreLoopManager.Instance.CreateGame(false);
+                    CoreLoopManager.Instance.StartObjectShowDown();
+                    GlobalUIManager.Instance.ShowTimeBar = true;
+                    _subState = SubState.RunState;
+                    break;
+                case SubState.RunState:
+                    if (Input.GetMouseButtonDown(0) && !TransitionManager.Instance.GetInTransition())
+                    {
+                        CoreLoopManager.Instance.GoToNextObject();
+                    }
+
+                    if (!TransitionManager.Instance.GetInTransition() && CoreLoopManager.Instance.TimeOut)
+                    {
+                        ChangeGameState(GameState.RoundScore, 2f, SceneName.None, false, TransitionMessage.TimeOut);
+                        CoreLoopManager.Instance.ResetShowDown();
+                    }
+
+                    if (CoreLoopManager.Instance.ShowDownCompleted)
+                    {
+                        _subState = SubState.ExitState;
+                    }
+                    break;
+                case SubState.ExitState:
+                    CoreLoopManager.Instance.ResetShowDown();
+                    ChangeGameState(GameState.PickUp, 2f);
+                    break;
+            }
+        }
+
+        private void PickUpState()
+        {
+            switch (_subState)
+            {
+                case SubState.BeginState:
+                    if (TransitionManager.Instance.GetInMiddlePoint())
+                    {
+                        CoreLoopManager.Instance.CreateModel(ModelCreationAlgorithm.ScalingRectangle);
+                        CoreLoopManager.Instance.CreateVictoryLine();
+                        CoreLoopManager.Instance.UpdateSlots();
+                        _subState = SubState.RunState;
+                    }
+                    break;
+                case SubState.RunState:
+                    if (!TransitionManager.Instance.GetInTransition() && CoreLoopManager.Instance.TimeOut)
+                    {
+                        CoreLoopManager.Instance.TestLine();
+                        ChangeGameState(GameState.RoundScore, 2f, SceneName.None, false, TransitionMessage.TimeOut);
+                        CoreLoopManager.Instance.CleanPickUp();
+                    }
+
+                    if (CoreLoopManager.Instance.LineComplete)
+                    {
+                        CoreLoopManager.Instance.TestLine();
+                        _subState = SubState.ExitState;
+                    }
+                    break;
+                case SubState.ExitState:
+                    CoreLoopManager.Instance.CleanPickUp();
+                    StateManager.Instance.ChangeGameState(GameState.ModelShowDown, 1f);
+                    break;
+            }
+        }
+
+        private void RoundScoreState()
+        {
+            switch (_subState)
+            {
+                case SubState.BeginState:
+                    GlobalUIManager.Instance.ShowTimeBar = false;
+                    CoreLoopManager.Instance.ShowEndScore();
+                    _subState = SubState.RunState;
+                    break;
+                case SubState.RunState:
+                    if (CoreLoopManager.Instance.StartNewGame)
+                    {
+                        _subState = SubState.ExitState;
+                    }
+                    break;
+                case SubState.ExitState:
+                    CoreLoopManager.Instance.ExitScoreView();
+                    ChangeGameState(GameState.DifficultySelection, 2f, SceneName.DifficultySelection, false, TransitionMessage.None);
+                    break;
+            }
+        }
+
+        public GameState GetGameState()
+        {
+            return _currentGameState;
+        }
+    }
+}
+
+*/
+
--- src/ApprienUnitySDK/Assets/GeoDefence/Scripts/Singletons/TransitionManager.cs
+++ src/ApprienUnitySDK/Assets/GeoDefence/Scripts/Singletons/TransitionManager.cs
@@ -2,17 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransitionManager : MonoBehaviour
+namespace GeoDefence
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TransitionManager : MonoBehaviour
     {
-        
-    }
+        public static TransitionManager Instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace gbpickup
+{
+    public class TransitionManager : MonoBehaviour
+    {
+        public static TransitionManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        [SerializeField] private AnimationCurve _transitionCurve;
+        [SerializeField] private Image _transitionPanelImage;
+        [SerializeField] private GameObject _getReadyMessage;
+        [SerializeField] private GameObject _timeOutMessage;
+        [SerializeField] private GameObject _transitionParticles;
+        private bool _inTransition;
+        private bool _inMiddlePoint;
+        private float _timer;
+        private float _duration;
+        private TransitionMessage _transitionMessage;
+
+        public void OrderTransition(float time, TransitionMessage message, bool particles)
+        {
+            _inTransition = true;
+            _transitionMessage = message;
+            _duration = time;
+            _transitionParticles.SetActive(particles);
+        }
+
+        private void Update()
+        {
+            if (_inTransition)
+            {
+                //_timer += _duration/Time.deltaTime;
+                _timer += Time.deltaTime;
+
+                if (_timer > _duration / 2)
+                {
+                    _inMiddlePoint = true;
+                    if (_transitionMessage == TransitionMessage.GetReady)
+                    {
+                        _getReadyMessage.SetActive(true);
+                        _timeOutMessage.SetActive(false);
+                    }
+                    if (_transitionMessage == TransitionMessage.TimeOut)
+                    {
+                        _getReadyMessage.SetActive(false);
+                        _timeOutMessage.SetActive(true);
+                    }
+                }
+
+                if (_timer > _duration)
+                {
+                    print("Setting Back to false");
+                    _inTransition = false;
+                    _timer = 0;
+                    _inMiddlePoint = false;
+                    _transitionMessage = TransitionMessage.None;
+                    _getReadyMessage.SetActive(false);
+                    _timeOutMessage.SetActive(false);
+                    _transitionParticles.SetActive(false);
+                }
+
+                _transitionPanelImage.color = new Color(_transitionPanelImage.color.r,
+                    _transitionPanelImage.color.g,
+                    _transitionPanelImage.color.b,
+                    _transitionCurve.Evaluate(_timer / _duration));
+            }
+        }
+
+        public bool GetInTransition()
+        {
+            return _inTransition;
+        }
+
+        public bool GetInMiddlePoint()
+        {
+            return _inMiddlePoint;
+        }
+    }
+}
+*/
+
